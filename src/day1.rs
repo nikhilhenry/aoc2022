d67b847c26b2a20acd900227126257b7644f1dd9
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-12-01 13:01:19 +0530

    complete part2

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use aoc;
 use itertools::Itertools;
-fn max_calories() -> Result<u32> {
+fn max_calories(top: usize) -> Result<u32> {
     Ok(aoc::read_one_per_line::<String>("./data/day1.input")?
         .into_iter()
         .group_by(|elt| elt.trim() != "")
@@ -10,10 +10,12 @@ fn max_calories() -> Result<u32> {
         .map(|(_ge0, group)| group.map(|el| el.parse::<u32>().unwrap()).collect_vec())
         .map(|g| g.iter().sum::<u32>())
         .sorted()
-        .last()
-        .unwrap())
+        .rev()
+        .take(top)
+        .sum())
 }
 fn main() -> Result<()> {
-    println!("Part 1: {}", max_calories()?);
+    println!("Part 1: {}", max_calories(1)?);
+    println!("Part 2: {}", max_calories(3)?);
     Ok(())
 }
