c8ce57d80322da287aa27d18659d3a46f7f24ecc
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-12-03 15:09:15 +0530

    refactor part 1

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -3,37 +3,36 @@ use aoc;
 use itertools::Itertools;
 use std::collections::HashSet;
 
-fn main() -> Result<()> {
-    let mut total_score = 0;
-    let lines = aoc::read_one_per_line::<String>("./data/day3.input")?;
-    for line in lines {
-        let mut set_1: HashSet<char> = HashSet::new();
-        let split_1 = &line[0..line.len() / 2];
-        for ch in split_1.chars() {
-            set_1.insert(ch);
-        }
-        let mut set_2: HashSet<char> = HashSet::new();
-        let split_2 = &line[line.len() / 2..line.len()];
-        for ch in split_2.chars() {
-            set_2.insert(ch);
-        }
-        let mut sack: Vec<char> = Vec::new();
-        sack.extend(set_1);
-        sack.extend(set_2);
-        let item = sack.into_iter().duplicates().collect_vec();
-        if item == vec![] {
-            continue;
-        }
-        let item = item[0];
-        //println!("{:?}", item);
-        let score: u32;
-        if item.is_uppercase() {
-            score = item as u32 - 64 + 26;
+trait Priority {
+    fn compute_priority(&self) -> u32;
+}
+
+impl Priority for char {
+    fn compute_priority(&self) -> u32 {
+        if self.is_uppercase() {
+            return *self as u32 - 64 + 26;
         } else {
-            score = item as u32 - 96;
+            return *self as u32 - 96;
         }
-        total_score += score;
     }
-    println!("{}", total_score);
+}
+
+fn total_priority() -> Result<u32> {
+    Ok(aoc::read_one_per_line::<String>("./data/day3.sample")?
+        .iter()
+        .map(|line| line.split_at(line.len() / 2))
+        .map(|(split_1, split_2)| {
+            (
+                split_1.chars().collect::<HashSet<char>>(),
+                split_2.chars().collect::<HashSet<char>>(),
+            )
+        })
+        .flat_map(|(split_1, split_2)| split_1.into_iter().merge(split_2).duplicates().nth(0))
+        .map(|ch| ch.compute_priority())
+        .sum())
+}
+
+fn main() -> Result<()> {
+    println!("Part 1:{}", total_priority()?);
     Ok(())
 }
