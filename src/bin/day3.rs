26284dbcc729775d650720676dfc001af7a86f1a
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-12-03 15:11:52 +0530

    refactor

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -18,16 +18,18 @@ impl Priority for char {
 }
 
 fn total_priority() -> Result<u32> {
-    Ok(aoc::read_one_per_line::<String>("./data/day3.sample")?
+    Ok(aoc::read_one_per_line::<String>("./data/day3.input")?
         .iter()
         .map(|line| line.split_at(line.len() / 2))
-        .map(|(split_1, split_2)| {
-            (
-                split_1.chars().collect::<HashSet<char>>(),
-                split_2.chars().collect::<HashSet<char>>(),
-            )
+        .flat_map(|(split_1, split_2)| {
+            split_1
+                .chars()
+                .collect::<HashSet<char>>()
+                .into_iter()
+                .merge(split_2.chars().collect::<HashSet<char>>())
+                .duplicates()
+                .nth(0)
         })
-        .flat_map(|(split_1, split_2)| split_1.into_iter().merge(split_2).duplicates().nth(0))
         .map(|ch| ch.compute_priority())
         .sum())
 }
