230603fb93e43b80b2c8cd497780d66721303458
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-12-04 12:11:22 +0530

    complete part 2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -18,14 +18,31 @@ impl TupleInt for (&str, &str) {
 
 trait RangeComparable {
     fn contains_within(self: &Self, other: &RangeInclusive<u32>) -> bool;
+    fn overlaps(self: &Self, other: &RangeInclusive<u32>) -> bool;
 }
 
 impl RangeComparable for RangeInclusive<u32> {
     fn contains_within(self: &Self, other: &RangeInclusive<u32>) -> bool {
         self.contains(&other.start()) && self.contains(&other.end())
     }
+    fn overlaps(self: &Self, other: &RangeInclusive<u32>) -> bool {
+        self.start() <= other.end() && other.start() <= self.end()
+    }
 }
 fn count_overlaps() -> Result<usize> {
+    Ok(aoc::read_one_per_line::<String>("./data/day4.input")?
+        .iter()
+        .flat_map(|block| block.split_once(","))
+        .filter(|(r1, r2)| {
+            let r1 = r1.split_once("-").unwrap().parse_int_tuple();
+            let r2 = r2.split_once("-").unwrap().parse_int_tuple();
+            let r1 = [r1.0..=r1.1];
+            let r2 = [r2.0..=r2.1];
+            r1[0].overlaps(&r2[0]) || r2[0].overlaps(&r1[0])
+        })
+        .count())
+}
+fn count_contains() -> Result<usize> {
     Ok(aoc::read_one_per_line::<String>("./data/day4.input")?
         .iter()
         .flat_map(|block| block.split_once(","))
@@ -41,6 +58,7 @@ fn count_overlaps() -> Result<usize> {
 }
 
 fn main() -> Result<()> {
-    println!("Part 1: {}", count_overlaps()?);
+    println!("Part 1: {}", count_contains()?);
+    println!("Part 2: {}", count_overlaps()?);
     Ok(())
 }
