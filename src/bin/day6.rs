e99fdac7c45b0d89e9118eb8e7b6d7729409582b
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-12-06 11:27:09 +0530

    clean up

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,17 +1,25 @@
 use itertools::Itertools;
 use std::collections::HashSet;
 
+fn find_marker(s: &Vec<char>, size: usize) -> Option<usize> {
+    for idx in 0..s.len() {
+        if idx + size == s.len() - 1 {
+            break;
+        }
+        let marker: HashSet<char> = s[idx..idx + size].iter().map(|c| *c).collect();
+        if marker.len() == size {
+            return Some(idx + size);
+        }
+    }
+    return None;
+}
 fn main() {
     let input = include_str!("../../data/day6.input").trim();
     let input = input.chars().collect_vec();
-    for idx in 0..input.len() {
-        if idx + 14 == input.len() - 1 {
-            break;
-        }
-        let marker: HashSet<char> = input[idx..idx + 14].iter().map(|c| *c).collect();
-        if marker.len() == 14 {
-            println!("Part 1: {}", idx + 14);
-            break;
-        }
+    if let Some(ans) = find_marker(&input, 4) {
+        println!("Part 1: {}", ans);
+    }
+    if let Some(ans) = find_marker(&input, 14) {
+        println!("Part 1: {}", ans);
     }
 }
